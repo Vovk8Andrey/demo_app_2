8a4452786879d040d7e387928432d456ef949d78
Andrey Vovk <89335673+Vovk8Andrey@users.noreply.github.com>
2023-05-08 20:11:58 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ import os
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings, Secret
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) #получаем urm нашей директории, где находится наш проект
-root_dir = dir_path[:-3] #то, где находится, наша бд
+dir_path = os.path.dirname(os.path.realpath(__file__)) 
+root_dir = dir_path[:-3] 
 config = Config(f'{root_dir}.env')
 
 
